5e718e8bf13a9c144cabf6bd2e0e8745006af759
mavisampaio19 <maria.sampaio.rodrigues@escola.pr.gov.br>
2025-10-22 09:30:39 -0300

    projeto aventura

---
+++ script.js
@@ -0,0 +1,10 @@
+const avanca - document.querySelectorA11('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener ('click', function(){
+       const atual = document.querySelector('.ativo')
+       const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+       atual.classList.remove('ativo');
+    })
+})
